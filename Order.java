f0cc4d6a4f2d83220103b9e9b301974e2b6b3e6a
yue002 <114208281+yue002@users.noreply.github.com>
2025-02-21 20:59:30 +0700

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    private String orderId;
+
+    public Order(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+}
