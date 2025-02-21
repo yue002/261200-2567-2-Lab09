2a7da4b9beecd29aebaa29974bb20164940b397d
yue002 <114208281+yue002@users.noreply.github.com>
2025-02-21 20:57:02 +0700

    Create Eagle.java

---
+++ Eagle.java
@@ -0,0 +1,10 @@
+public class Eagle implements Flyable{
+    @Override
+    public void move() {
+        System.out.println("อินทรีเดิน!!!!");
+    }
+    @Override
+    public void fly() {
+        System.out.println("พรึ่บ! อินทรีบินอย่างเท่");
+    }
+}
