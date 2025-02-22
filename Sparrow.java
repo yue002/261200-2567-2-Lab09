d80efb00279368d001003c83fc4d7beeeb1a91cb
yue002 <114208281+yue002@users.noreply.github.com>
2025-02-22 15:44:11 +0700

    Create Sparrow.java

---
+++ Sparrow.java
@@ -0,0 +1,10 @@
+public class Sparrow implements Flyable {
+    @Override
+    public void move() {
+        System.out.println("น้องกระจอกเดินต๊อกแต๊ก");
+    }
+    @Override
+    public void fly() {
+        System.out.println("weeeew น้องกระจอกบินไปไหน");
+    }
+}
